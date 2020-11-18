3289ac17a316dd465ab14ec42f574f30a9e95acb
Amitesh Krishna <anantk124@gmail.com>
2020-11-18 13:59:23 +0530

    Updated as of Class-37

--- Food.js
+++ Food.js
@@ -1,7 +1,6 @@
 class Food{
 	constructor(){
 		this.foodS = 0;
-		this.lastFed = 0;
 		this.image = loadImage("images/Milk.png");
 	}
 
@@ -22,13 +21,13 @@ class Food{
 	}
 
 	display(){
-		var x = 80,y=100;
-		image(this.image,720,220,70,70);
+		var x = 0,y=100;
+		image(this.image,300,220,70,70);
 
 		if(this.foodS!=0){
 			for(var i = 0;i<this.foodS;i++){
 				if(i%10 == 0){
-					x = 80;
+					x = 0;
 					y += 50;
 				}
 				image(this.image,x,y,50,50);
--- sketch.js
+++ sketch.js
@@ -1,17 +1,23 @@
 var dogSpr,dog,happyDog;
 var allFoods,foodStock = 0;
 var addFood,feedDog,feedTime = null;
+var gameState,databaseRef;
+var bedImg,gardenImg,toiletImg;
+var bgImg = null;
 var database;
 
 function preload()
 {
     dog = loadImage("images/dogImg.png");
     happyDog = loadImage("images/dogImg1.png");
+    bedImg = loadImage("images/Bed Room.png");
+    gardenImg = loadImage("images/Garden.png");
+    toiletImg = loadImage("images/Wash Room.png");
 }
 
 function setup() {
-    createCanvas(1000,500);
-    dogSpr = createSprite(width-140,height/2);
+    createCanvas(500,500);
+    dogSpr = createSprite(width-80,height/2);
     dogSpr.scale = 0.2;
     dogSpr.addImage("fed",happyDog);
     dogSpr.addImage("standing",dog);
@@ -26,40 +32,80 @@ function setup() {
         allFoods.updateFoodStock(data.val());
     });
     
+    var feedTimeRef = database.ref("FeedTime");
+    feedTimeRef.once("value",(data)=>{
+        feedTime = data.val();
+    });
+
+    database.ref("gameState").once("value",(data)=>{
+        gameState = data.val();
+    });
+
+    databaseRef = database.ref("/");
+
     addFood = createButton("Add Food");
-    addFood.position(700,95);
+    addFood.position(500,95);
     addFood.mousePressed(addFoods);
 
     feedDog = createButton("Feed Dog");
-    feedDog.position(800,95);
+    feedDog.position(600,95);
     feedDog.mousePressed(FeedDog);
 
     allFoods.getFoodStock();
 }
 
 
-function draw() {  
-    background(color(46,139,87));
+function draw() { 
+    if(bgImg != null){
+        background(bgImg);
+    }else{
+        background(color(46,139,87));
+    }
     drawSprites();
-    allFoods.display();
 
+    if(feedTime == hour()){
+        dogSpr.changeImage("fed");
+    }
+
+    if(gameState != "hungry"){
+        addFood.hide();
+        feedDog.hide();
+    }else{
+        addFood.show();
+        feedDog.show();
+    }
+
+    if(hour() == feedTime+1){
+        Garden();
+        gameState = "playing";
+        databaseRef.update({gameState:"playing"});
+    }else if(hour() == feedTime+2){
+        bedRoom();
+        gameState = "sleeping";
+        databaseRef.update({gameState:"sleeping"});
+    }else if(hour() == feedTime+3||hour() == feedTime+4){
+        WashRoom();
+        gameState = "bathing";
+        databaseRef.update({gameState:"bathing"});
+    }else{
+        gameState = "hungry";
+        databaseRef.update({gameState:"hungry"});
+        allFoods.display();
+        dogSpr.visible = true;
+        bgImg = null
+    }
+
+    // Last feeding time checking
     textSize(20);
     fill(255,255,254);
     strokeWeight(1);
     stroke("Black");
-    
     if(feedTime>12){
-        text("Last fed on " + (feedTime - 12) + "PM",350,30);
+        text("Last fed on " + (feedTime - 12) + "PM",150,30);
     }else if(feedTime == 0){
-        text("Last fed on 12AM",350,30);
-    }else if(feedTime==null){
-        text("Feed it first!",350,30);
+        text("Last fed on 12AM",150,30);
     }else{
-        text("Last fed on "+feedTime+"AM",350,30);
-    }
-
-    if(feedTime!=null){
-        database.ref("/").update({FeedTime:feedTime})
+        text("Last fed on "+feedTime+"AM",150,30);
     }
 }
 
@@ -69,5 +115,22 @@ function addFoods(){
 
 function FeedDog(){
     dogSpr.changeImage("fed");
-    allFoods.deductFood();feedTime = hour();
+    allFoods.deductFood();
+    feedTime = hour();
+    databaseRef.update({FeedTime:feedTime});
+}
+
+function bedRoom(){
+    bgImg = bedImg;
+    dogSpr.visible = false;
+}
+
+function Garden(){
+    bgImg = gardenImg;
+    dogSpr.visible = false;
+}
+
+function WashRoom(){
+    bgImg = toiletImg;
+    dogSpr.visible = false;
 }
